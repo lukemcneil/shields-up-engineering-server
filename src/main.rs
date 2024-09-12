71aaaafa0049f720ff0e630f28d5834958ebb496
Luke McNeil <lukemcneil61@gmail.com>
2024-09-12 18:13:45 -0500

    games now persist on server

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 #[macro_use]
 extern crate rocket;
 
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
+use std::{collections::HashMap, sync::Arc};
 
 use game::{GameState, UserActionError, UserActionWithPlayer};
-use rocket::{get, State};
+use rocket::{futures::lock::Mutex, get, State};
 
 mod cards;
 mod client;
@@ -15,45 +12,38 @@ mod game;
 mod tests;
 
 #[get("/game/<game_name>")]
-fn play_game(
+async fn play_game(
     ws: ws::WebSocket,
     game_name: &str,
     games_state: &State<Arc<Mutex<Games>>>,
 ) -> ws::Channel<'static> {
-    let mut game_state = GameState::start_state();
-    game_state.player1.life_support.hot_wires = vec![game_state.deck.pop().unwrap()];
-    game_state.player1.shield_generator.hot_wires = vec![
-        game_state.deck.pop().unwrap(),
-        game_state.deck.pop().unwrap(),
-    ];
-
-    game_state.player2.weapons_system.hot_wires = vec![
-        game_state.deck.pop().unwrap(),
-        game_state.deck.pop().unwrap(),
-    ];
-
-    let mut games = games_state.lock().unwrap();
+    let mut games = games_state.lock().await;
     if !games.0.contains_key(game_name) {
         games
             .0
-            .insert(game_name.to_string(), Arc::new(Mutex::new(game_state)));
+            .insert(game_name.to_string(), GameState::start_state());
     }
 
+    let games_state = Arc::clone(games_state);
+    let game_name = game_name.to_string();
     use rocket::futures::{SinkExt, StreamExt};
     ws.channel(move |mut stream| {
         Box::pin(async move {
-            let mut game_state = GameState::start_state(); // TODO: get the game state to send initial state
+            let mut games = games_state.lock().await;
+            let game_state = games.0.get_mut(&game_name).unwrap();
             let _ = stream
                 .send(ws::Message::Text(
                     serde_json::to_string(&game_state).unwrap(),
                 ))
                 .await;
+            drop(games);
             while let Some(message) = stream.next().await {
                 if let ws::Message::Text(text) = message? {
                     println!("received: {}", text);
                     match serde_json::from_str::<UserActionWithPlayer>(&text) {
                         Ok(user_action_with_player) => {
-                            // TODO: probably need to do something in here to lock on the mutex for the game state to apply the action to the state and send back the new state
+                            let mut games = games_state.lock().await;
+                            let game_state = games.0.get_mut(&game_name).unwrap();
                             let result = game_state.receive_user_action(user_action_with_player);
                             let _ = stream
                                 .send(ws::Message::Text(serde_json::to_string(&result).unwrap()))
@@ -99,7 +89,7 @@ fn test() -> String {
 }
 
 #[derive(Default)]
-struct Games(HashMap<String, Arc<Mutex<GameState>>>);
+struct Games(HashMap<String, GameState>);
 
 #[launch]
 fn rocket() -> _ {
